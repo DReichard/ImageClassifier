71d4ebcdf526750edfd04caa0ef5bbb343c6c1ab
DReichard <stanislav1996@mail.ru>
2019-03-19 22:44:11 +0300

    hello world diagnostics

--- rdn_assembly.py
+++ rdn_assembly.py
@@ -1,5 +1,6 @@
-from keras import models
+from keras import models, Input, Model
 from keras import layers
+from keras.layers import Conv2D, ReLU, Dense
 from keras.layers.normalization import BatchNormalization
 
 
@@ -7,20 +8,47 @@ def add_second_type(network, size):
     print('WIP')
 
 
-def add_first_type(network, size):
-    network.add(layers.Conv2D(size, kernel_size=(3, 3), input_shape=(size*4, size*4, 1)))
+def first_type_layer2(network, size):
+    network.add(layers.Conv2D(size, kernel_size=(3, 3), input_shape=(size * 4, size * 4, 1)))
     network.add(BatchNormalization())
     network.add(layers.ReLU())
 
 
+def first_type_layer(input_value, size):
+    convolution_layer = Conv2D(size, kernel_size=(3, 3))(input_value)
+    batch_normalized_layer = BatchNormalization(axis=1)(convolution_layer)
+    activation_layer = ReLU()(batch_normalized_layer)
+    return activation_layer
+
+
 def get_network():
-    network = models.Sequential()
-    add_first_type(network, 64)
-    add_first_type(network, 16)
-    # add_first_type(network, 16)
+    input_value = Input(shape=(256, 256))
+    x = first_type_layer(input_value, 64)
+    x = first_type_layer(x, 16)
+
+    output = Dense(2, activation='softmax')(x)
+
+    network = Model(inputs=input_value, outputs=output)
     network.compile(optimizer='rmsprop',
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
     print('Network Compiled')
     return network
 
+
+def get_network_test():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 1)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(2, activation='sigmoid'))
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    print('Network Compiled')
+    return model
--- train.py
+++ train.py
@@ -1,19 +1,22 @@
 from keras import models
 from keras import layers
+import numpy as np
+import image_parsing
 import prepare_dataset
 import rdn_assembly
+from PIL import Image
 
 clean_path = 'D:\\nir_datasets\\jpg\\clean\\clean_cut_images1'
 affected_path = 'D:\\nir_datasets\\jpg\\affected\\sorted\\vsl_lsb_5kb\\2019-03-17-15-04-40-207\\input000';
-dataset_limit = 3000
+dataset_limit = 2000
 
 images, labels = prepare_dataset.get_dataset(clean_path, affected_path, dataset_limit)
+images = images.astype('float32')/255
 images_train, images_test, labels_train, labels_test = prepare_dataset.split_dataset(images, labels)
 
-network = rdn_assembly.get_network()
-
+network = rdn_assembly.get_network_test()
 print('Fitting...')
-network.fit(images_train, labels_train, epochs=5, batch_size=1500)
+network.fit(images_train, labels_train, epochs=5, batch_size=30)
 
 print('Testing...')
 # test_loss, test_acc = network.evaluate(images_test, labels_test)
